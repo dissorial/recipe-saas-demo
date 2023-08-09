90a9dc924d905bd0f7ba66f65d59d253390d52e4
dissorial <87335884+dissorial@users.noreply.github.com>
2023-08-09 23:24:27 +0200

    Revert "lk"
    This reverts commit 3addc544a9ef83e5b2dab5d9cf47c27679c3b140.

--- app/page.tsx
+++ app/page.tsx
@@ -1,124 +1,32 @@
-"use client"
-
-import { useState } from "react"
-import Image from "next/image"
-import { Subscription } from "@/types"
-import { zodResolver } from "@hookform/resolvers/zod"
-import { useForm } from "react-hook-form"
-import * as z from "zod"
+import Link from "next/link"
 
 import { Button } from "@/components/ui/button"
 import {
-  Form,
-  FormControl,
-  FormField,
-  FormItem,
-  FormMessage,
-} from "@/components/ui/form"
-import { Textarea } from "@/components/ui/textarea"
-import { LoadingPlaceholder } from "@/components/empty-placeholders"
+  PageHeader,
+  PageHeaderDescription,
+  PageHeaderHeading,
+} from "@/components/page-header"
 
-const FormSchema = z.object({
-  query: z.string(),
-})
-
-export default function SomePage({
-  subscription,
-}: {
-  subscription: Subscription | null
-}) {
-  const [imageData, setImageData] = useState<string>()
-
-  const form = useForm<z.infer<typeof FormSchema>>({
-    resolver: zodResolver(FormSchema),
-  })
-
-  const isLoading = form.formState.isSubmitting
-
-  async function onSubmit(data: z.infer<typeof FormSchema>) {
-    const query = data.query
-    try {
-      const response = await fetch(`/api/dezgo`, {
-        method: "POST",
-        body: JSON.stringify({ query }),
-      })
-      const blob = await response.blob()
-      const objectURL = URL.createObjectURL(blob)
-
-      setImageData(objectURL)
-    } catch (error) {
-      console.log(error)
-    }
-  }
-
-  console.log("imagedata", typeof imageData)
-
+export default function IndexPage() {
   return (
-    <div className="border-t">
-      <div className="bg-background">
-        <div className="grid lg:grid-cols-5">
-          <div className="col-span-3 lg:col-span-5 lg:border-l">
-            <div className="h-full px-4 py-6 lg:px-8">
-              <div className="h-full space-y-6">
-                <div className="space-between flex items-center">
-                  <div className="mx-auto w-4/5 space-y-6 sm:w-1/2 lg:w-1/3">
-                    <h2 className="text-center text-2xl font-semibold tracking-tight">
-                      Generate images
-                    </h2>
-                    <Form {...form}>
-                      <form
-                        onSubmit={form.handleSubmit(onSubmit)}
-                        className="space-y-3"
-                      >
-                        <FormField
-                          control={form.control}
-                          name="query"
-                          render={({ field }) => (
-                            <FormItem>
-                              <FormControl>
-                                <Textarea
-                                  placeholder="Astronaut on the moon"
-                                  className="resize-none"
-                                  {...field}
-                                />
-                              </FormControl>
-                              <FormMessage />
-                            </FormItem>
-                          )}
-                        />
-                        <Button type="submit" className="w-full">
-                          Generate
-                        </Button>
-                      </form>
-                    </Form>
-                  </div>
-                </div>
-
-                <div className="border-none p-0 outline-none">
-                  <div className="mx-auto w-full">
-                    {isLoading ? (
-                      <LoadingPlaceholder />
-                    ) : (
-                      <>
-                        {imageData && (
-                          <>
-                            <Image
-                              src={imageData}
-                              alt="Generated image"
-                              width={300}
-                              height={300}
-                            />
-                          </>
-                        )}
-                      </>
-                    )}
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
+    <div className="container relative flex h-screen flex-col items-center justify-center">
+      <PageHeader className="pb-8">
+        <div className="inline-flex items-center rounded-lg bg-muted px-3 py-1 text-sm font-medium">
+          <span className="sm:inline">
+            Featuring detailed nutritional information
+          </span>
         </div>
-      </div>
+        <PageHeaderHeading>Find the perfect recipes</PageHeaderHeading>
+        <PageHeaderDescription className="mt-4">
+          Search through a curated list of low-carb/keto recipes and save
+          valuable time.
+        </PageHeaderDescription>
+        <div className="pb-8 pt-4 md:pb-10">
+          <Button asChild>
+            <Link href="/sign-in">Get started</Link>
+          </Button>
+        </div>
+      </PageHeader>
     </div>
   )
 }
